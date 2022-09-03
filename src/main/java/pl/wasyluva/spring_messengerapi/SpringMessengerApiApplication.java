e473c9418b86cdb2c7be2654cb246b6aba6388c9
Marek Wasyluk <marekwasyluk@wp.pl>
2022-09-03 08:01:20 +0200

    feat: Add PasswordEncoder and encode static password
    refactor: Change default query for getting authorities from DB

--- src/main/java/pl/wasyluva/spring_messengerapi/SpringMessengerApiApplication.java
+++ src/main/java/pl/wasyluva/spring_messengerapi/SpringMessengerApiApplication.java
@@ -5,10 +5,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import pl.wasyluva.spring_messengerapi.data.repository.UserRepository;
 import pl.wasyluva.spring_messengerapi.domain.userdetails.UserAuthority;
 import pl.wasyluva.spring_messengerapi.domain.userdetails.UserDetails;
 
+import javax.sql.DataSource;
 import java.util.Arrays;
 import java.util.Collections;
 
@@ -26,7 +29,7 @@ public class SpringMessengerApiApplication {
 	@Bean
 	public CommandLineRunner run() {
 		return (arg) -> {
-			UserDetails userDetails = new UserDetails("admin", "password", Arrays.asList(UserAuthority.USER, UserAuthority.ADMIN));
+			UserDetails userDetails = new UserDetails("admin", "{bcrypt}$2a$12$DMm64rBiqoiI.gARFG5mJOBotWkrazCq.0v4iwQmgj4hmpQ010zcq", Arrays.asList(UserAuthority.USER, UserAuthority.ADMIN));
 			userRepository.save(userDetails);
 		};
 	}
--- src/main/java/pl/wasyluva/spring_messengerapi/security/SecurityConfig.java
+++ src/main/java/pl/wasyluva/spring_messengerapi/security/SecurityConfig.java
@@ -1,8 +1,32 @@
 package pl.wasyluva.spring_messengerapi.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder (){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
+    @Bean
+    public UserDetailsService jdbcUserDetailsService(DataSource dataSource) {
+        String usersByUsernameQuery = "SELECT username, password, enabled FROM users WHERE username = ?";
+        String authsByUsernameQuery = "SELECT username, authority FROM users_authorities WHERE username = ?";
+
+        JdbcUserDetailsManager usersManager = new JdbcUserDetailsManager(dataSource);
+
+        usersManager.setUsersByUsernameQuery (usersByUsernameQuery);
+        usersManager.setAuthoritiesByUsernameQuery (authsByUsernameQuery);
+
+        return usersManager;
+    }
 }
