c53874c237b9503c9752645dc46cdafc4ce1c80d
Marek Wasyluk <marekwasyluk@wp.pl>
2022-09-09 08:20:37 +0200

    feat: Add 2 endpoints for testing purposes

---
+++ src/main/java/pl/wasyluva/spring_messengerapi/web/controller/HomeController.java
@@ -0,0 +1,17 @@
+package pl.wasyluva.spring_messengerapi.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String getHome(){
+        return "Welcome at the main endpoint! (unsecured)";
+    }
+
+    @GetMapping("/test")
+    public String getTest(){
+        return "Welcome at the Test endpoint!";
+    }
+}
