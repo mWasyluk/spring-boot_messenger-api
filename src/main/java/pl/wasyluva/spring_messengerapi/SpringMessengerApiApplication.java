207e3134a9c4558d96c63f1184d1501eae42bcf1
Marek Wasyluk <marekwasyluk@wp.pl>
2022-09-11 14:51:01 +0200

    dev: Add sample data to the DB

--- src/main/java/pl/wasyluva/spring_messengerapi/SpringMessengerApiApplication.java
+++ src/main/java/pl/wasyluva/spring_messengerapi/SpringMessengerApiApplication.java
@@ -1,21 +1,22 @@
 package pl.wasyluva.spring_messengerapi;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import pl.wasyluva.spring_messengerapi.data.repository.UserRepository;
+import pl.wasyluva.spring_messengerapi.data.repository.UserDetailsRepository;
+import pl.wasyluva.spring_messengerapi.data.repository.UserProfileRepository;
+import pl.wasyluva.spring_messengerapi.domain.userdetails.UserProfile;
 import pl.wasyluva.spring_messengerapi.domain.userdetails.UserAuthority;
 import pl.wasyluva.spring_messengerapi.domain.userdetails.UserDetails;
 
-import javax.sql.DataSource;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.Calendar;
 
 @SpringBootApplication
+@Slf4j
 public class SpringMessengerApiApplication {
 
 	public static void main(String[] args) {
@@ -24,13 +25,22 @@ public class SpringMessengerApiApplication {
 
 
 	@Autowired
-	private UserRepository userRepository;
+	private UserDetailsRepository userDetailsRepository;
+	@Autowired
+	private UserProfileRepository userProfileRepository;
 
 	@Bean
 	public CommandLineRunner run() {
 		return (arg) -> {
+			// Add a UserDetails object with the default admin user to the DB
 			UserDetails userDetails = new UserDetails("admin", "{bcrypt}$2a$12$psbR2EBlOAXlmrlMCpmSj.Wg/28HjOqRrgsHE1Ud0WTEwiJr5AVZu", Arrays.asList(UserAuthority.USER, UserAuthority.ADMIN));
-			userRepository.save(userDetails);
+			log.info("Added UserDetails with ID: '" + userDetailsRepository.save(userDetails).getId() + "' to the database.");
+
+			// Add two basic Users' profiles to the DB
+			UserProfile sourceUser = new UserProfile("Marek", "Wasyluk", new Calendar.Builder().setDate(1999, 5, 16).build().getTime());
+			UserProfile targetUser = new UserProfile("Jan", "Pasieka", new Calendar.Builder().setDate(1995, 3, 28).build().getTime());
+			log.info("Added User with ID: '" + userProfileRepository.save(sourceUser).getId() + "' to the database.");
+			log.info("Added User with ID: '" + userProfileRepository.save(targetUser).getId() + "' to the database.");
 		};
 	}
 
