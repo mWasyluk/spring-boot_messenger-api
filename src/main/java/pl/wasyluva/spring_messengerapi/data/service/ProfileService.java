60a52e536aeee9d590f4dba525024f6eddcad9f9
Marek Wasyluk <marekwasyluk@wp.pl>
2022-09-25 00:11:19 +0200

    feat: Create endpoint for Profile creation

--- src/main/java/pl/wasyluva/spring_messengerapi/data/service/ProfileService.java
+++ src/main/java/pl/wasyluva/spring_messengerapi/data/service/ProfileService.java
@@ -3,7 +3,9 @@ package pl.wasyluva.spring_messengerapi.data.service;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import pl.wasyluva.spring_messengerapi.data.repository.AccountRepository;
 import pl.wasyluva.spring_messengerapi.data.repository.ProfileRepository;
+import pl.wasyluva.spring_messengerapi.domain.userdetails.Account;
 import pl.wasyluva.spring_messengerapi.domain.userdetails.Profile;
 
 import java.util.List;
@@ -14,9 +16,11 @@ import java.util.UUID;
 @Slf4j
 public class ProfileService {
     private final ProfileRepository profileRepository;
+    private final AccountRepository accountRepository;
 
-    public ProfileService(ProfileRepository profileRepository) {
+    public ProfileService(ProfileRepository profileRepository, AccountRepository accountRepository) {
         this.profileRepository = profileRepository;
+        this.accountRepository = accountRepository;
     }
 
     // TODO: Return snips of profiles objects (DTO)
@@ -30,6 +34,23 @@ public class ProfileService {
     //  if false -> return a snip of the object (DTO)
     public Profile getProfileById(@NonNull UUID userId){
         return profileRepository.findById(userId).orElse(null);
+    }
+
+    public Profile createProfile(@NonNull UUID accountId, @NonNull Profile profile){
+        Optional<Account> byId = accountRepository.findById(accountId);
+        if (!byId.isPresent()){
+            log.debug("Account with ID " + accountId + " does not exist");
+            return null;
+        }
+
+        if (byId.get().getProfile() != null){
+            log.debug("Account with ID " + accountId + " already has Profile assigned");
+            return null;
+        }
+
+        Account account = byId.get();
+        account.setProfile(profile);
+        return accountRepository.save(account).getProfile();
     }
 
     // TODO: Add a requestingUser parameter to the method and check if he is the owner of the profile
