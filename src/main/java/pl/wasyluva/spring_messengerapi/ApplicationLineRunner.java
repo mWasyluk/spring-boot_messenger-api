1cb759ec06fbc50b7b3e666a8a62af5a40a6a396
Marek Wasyluk <marekwasyluk@wp.pl>
2023-01-10 21:31:20 +0100

    refactor: disable creating a new Profile for the admin account on the app start-up

--- src/main/java/pl/wasyluva/spring_messengerapi/ApplicationLineRunner.java
+++ src/main/java/pl/wasyluva/spring_messengerapi/ApplicationLineRunner.java
@@ -5,11 +5,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import pl.wasyluva.spring_messengerapi.data.repository.AccountRepository;
 import pl.wasyluva.spring_messengerapi.domain.userdetails.Account;
-import pl.wasyluva.spring_messengerapi.domain.userdetails.Profile;
 import pl.wasyluva.spring_messengerapi.domain.userdetails.UserAuthority;
 
 import java.util.Arrays;
-import java.util.Calendar;
 
 @Configuration
 public class ApplicationLineRunner implements CommandLineRunner {
@@ -30,13 +28,7 @@ public class ApplicationLineRunner implements CommandLineRunner {
 						"admin",
 						"{bcrypt}$2a$12$psbR2EBlOAXlmrlMCpmSj.Wg/28HjOqRrgsHE1Ud0WTEwiJr5AVZu",
 						Arrays.asList(UserAuthority.USER, UserAuthority.ADMIN));
-				// create a Profile and assign it to the Account
-				Profile profile1 = new Profile(
-						"Marek",
-						"Wasyluk",
-						new Calendar.Builder().setDate(1999, 5, 16).build().getTime());
 
-				account1.setProfile(profile1);
                 // persist the Account
 				Account savedUserWithProfile1 = accountRepository.save(account1);
 			}
